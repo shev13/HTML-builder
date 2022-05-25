9995f7a7632105a40feae21717c0bb577a96571d
Victor Shevchenko <13shev.js@gmail.com>
2022-05-25 08:28:12 +0300

    feat: tack-04 complete

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
+const fs = require('fs/promises');
+const path = require('path');
 
+async function copyDir() {
+  const rmDir = await fs.rm(path.join(__dirname, 'files-copy'), {
+    force: true,
+    recursive: true,
+  });
+
+  const newDir = await fs.mkdir(path.join(__dirname, 'files-copy'), {
+    recursive: true,
+  });
+  const files = await fs.readdir(path.join(__dirname, 'files'));
+  for (const file of files) {
+    const st = await fs.stat(path.join(__dirname, 'files', file));
+    if (st.isFile()) {
+      const newFile = await fs.copyFile(
+        path.join(__dirname, 'files', file),
+        path.join(__dirname, 'files-copy', file),
+      );
+    }
+  }
+}
+
+copyDir();
